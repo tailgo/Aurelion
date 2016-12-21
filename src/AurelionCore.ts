9eb0b0d37ed907cdbcdd97e21a041fbcda68e536
tailgo <zenosturtle@outlook.com>
2016-12-21 21:44:24 +0800

    divided aurelion into core and window

---
+++ src/AurelionCore.ts
@@ -0,0 +1,12 @@
+// import * as electron from 'electron';
+import { ipcMain, ipcRenderer } from 'electron';
+
+class AurelionCore {
+  constructor() {}
+
+  public startApp() {};
+}
+
+const Core: AurelionCore = new AurelionCore();
+
+export { Core };
---
+++ src/AurelionWindow.ts
@@ -0,0 +1,77 @@
+'use strict';
+import * as electron from 'electron';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as url from 'url';
+/**
+ * Math Classes
+ */
+export * from './Math/Color';
+export * from './Math/Vector2';
+export * from './Math/Vector3';
+
+global['__debug__'] = false;
+
+class AurelionWindow {
+  private _gConfig: JSON;
+  private _gDebug: boolean = false;
+
+  private _electronApp: Electron.App;
+  private _electronWindow: Electron.BrowserWindow;
+
+  constructor() {
+    if (process.argv.indexOf('--dg') > -1) {
+      global['__debug__'] = true;
+    }
+
+    this._electronApp = electron.app;
+  }
+
+  public init(config: string = 'aurelion.json') {
+    // load config file
+    let configPath: string = path.resolve(config);
+    this._gConfig = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+
+    if (global['__debug__']) {
+      console.log(`Init Core, config is ${JSON.stringify(this._gConfig)}`);
+    }
+  }
+
+  public run() {
+    if (global['__debug__']) {
+      console.log('Start Core Run.');
+    }
+
+    this._electronApp.on('ready', this._createWindow);
+    this._electronApp.on('activate', () => {
+      if (this._electronWindow === null) {
+        this._createWindow();
+      }
+    });
+  }
+
+  private _createWindow() {
+    this._electronWindow = new electron.BrowserWindow({
+      width: 800,
+      height: 600
+    });
+
+    this._electronWindow.loadURL(url.format({
+      pathname: path.join(__dirname, 'main.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+
+    if (global['__debug__']) {
+      this._electronWindow.webContents.openDevTools();
+    }
+
+    this._electronWindow.on('closed', () => {
+      this._electronWindow = null;
+    });
+  }
+}
+
+const ALWindow: AurelionWindow = new AurelionWindow();
+
+export { ALWindow };
